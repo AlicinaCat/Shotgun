9433bdd7f3bb7efcdbcee231c2ae1c7663ae66dc
alicinacat <alice.casaburi@gmail.com>
2018-11-21 16:28:19 +0100

    added player class, shoot, reload, block, shotgun

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function Player(name, shots) {
     this.name = name;
-    this.shots = 0;
+    this.shots = shots;
 }
+
+Player.prototype.reload = function() {
+    return this.shots += 1;
+}
+
+Player.prototype.shoot = function() {
+    return this.shots -= 1;
+}
+
+Player.prototype.block = function() {
+
+}
+
+Player.prototype.shotgun = function() {
+    return this.name + " won!";
+}
+
+let player = new Player('Alicina', 0)
+console.log("Player name is " + player.name + ", available shots: " + player.shots);
+
+player.reload();
+console.log(player.shots);
+player.reload();
+console.log(player.shots);
+player.shoot();
+console.log(player.shots);
+console.log(player.shotgun());
 
