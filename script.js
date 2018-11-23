efb9dd7cd18fab7d116adff4a42230bb12e6eb0c
alicinacat <alice.casaburi@gmail.com>
2018-11-23 11:56:41 +0100

    switched to even listener

--- script.js
+++ script.js
@@ -4,29 +4,49 @@ function Player(name, shots) {
 }
 
 Player.prototype.reload = function() {
-    return this.shots += 1;
+    console.log("Current shots: " + player.shots)
+    return player.shots += 1;
 }
 
 Player.prototype.shoot = function() {
-    return this.shots -= 1;
+    if (player.shots > 0)
+        return this.shots -= 1;
 }
 
 Player.prototype.block = function() {
-
 }
 
 Player.prototype.shotgun = function() {
     return this.name + " won!";
 }
 
-let player = new Player('Alicina', 0)
-console.log("Player name is " + player.name + ", available shots: " + player.shots);
+Player.prototype.check = function() {
+    if (player.shots == 3)
+        return player.shotgun();
+    else if (computer.shots == 3)
+        return computer.shotgun(); 
 
-player.reload();
-console.log(player.shots);
-player.reload();
-console.log(player.shots);
-player.shoot();
-console.log(player.shots);
-console.log(player.shotgun());
+}
+
+
+Player.prototype.random = function() {
+    let choice = Math.floor(Math.random() * 3);
+    console.log("choice: " + choice);
+    if (choice == 0) {
+        computer.reload();
+    } else if (choice == 1) {
+        computer.block();
+    } else if (choice == 2) {
+        computer.shoot();
+    }
+}
+
+let player = new Player('Alicina', 0);
+console.log("Player 1 name is " + player.name + ", available shots: " + player.shots);
+
+let computer = new Player('Computer', 0);
+console.log("Player 2 name is " + computer.name + ", available shots: " + computer.shots);
+
+var wrapButton = document.getElementById('wrap');
+wrapButton.addEventListener('click', player.reload);
 
