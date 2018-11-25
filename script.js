27686c3828738070ce7799de9d8bc1c8dc640996
alicinacat <alice.casaburi@gmail.com>
2018-11-25 12:30:22 +0100

    inserted pictures of characters and have basic functionality in place

--- script.js
+++ script.js
@@ -27,25 +27,47 @@ class Player {
         let choice = Math.floor(Math.random() * 3);
         console.log("choice: " + choice);
         if (choice == 0) {
-            reload();
+            this.reload();
         } else if (choice == 1) {
-            block();
+            this.block();
         } else if (choice == 2) {
-            shoot();
+            this.shoot();
         }
     }
 }
 
-function turn(choice) { 
-    console.log("choice: " + choice);
-    if (choice == 0) {
-        player.reload();
-    } else if (choice == 1) {
-        player.block();
-    } else if (choice == 2) {
-        player.shoot();
+function check() {
+    if (player.shots == 3) {
+        console.log(player.shotgun());
+        return true;
+    } else if (computer.shots == 3) {
+        console.log(computer.shotgun())
+        return true;
+    } else {
+        return false;
     }
+}
 
+function turn(choice) {
+    var gameOver = check();
+    console.log(gameOver);
+
+    if (!gameOver) {
+
+        console.log("choice: " + choice);
+        if (choice == 0) {
+            player.reload();
+        } else if (choice == 1) {
+            player.block();
+        } else if (choice == 2) {
+            player.shoot();
+        }
+
+        computer.random();
+
+        console.log("Player " + player.name + " has " + player.shots + " shots");
+        console.log("Player " + computer.name + " has " + computer.shots + " shots");
+    }
 }
 
 var player = new Player('Alicina', 0);
@@ -56,11 +78,11 @@ console.log("Player 2 name is " + computer.name + ", available shots: " + comput
 
 
 var wrapButton = document.getElementById('wrap');
-wrapButton.addEventListener('click', function() {
+wrapButton.addEventListener('click', function () {
     turn(0);
 });
 
 var throwButton = document.getElementById('throw');
-throwButton.addEventListener('click', function() {
+throwButton.addEventListener('click', function () {
     turn(2);
 });
